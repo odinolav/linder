eaa68563faaa581c415ae533d095194e6e0679d7
odinolav <odinolav@gmail.com>
2019-02-17 23:24:06 -0600

    Most components added, some styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,245 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'date-fns';
+import React, {Component} from 'react';
+import './css/App.css';
+import './css/reset.css';
+import { withStyles } from '@material-ui/core/styles';
+import withRoot from './withRoot';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { MuiPickersUtilsProvider, TimePicker, DatePicker } from 'material-ui-pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import Fab from '@material-ui/core/Fab';
+import { Navigation, Edit, ExpandMore } from '@material-ui/icons';
+import IconButton from '@material-ui/core/IconButton';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import classnames from 'classnames';
+//import InputAdornment from '@material-ui/core/InputAdornment';
+//import IconButton from '@material-ui/core/IconButton';
+
+
+import STRINGS from './text/Strings';
+
+function Transition(props) {
+  return <Slide direction='up' {...props} />;
+}
+
+const styles = theme => ({
+  root: {
+    textAlign: 'left',
+    paddingTop: theme.spacing.unit * 8,
+  },
+  card: {
+    maxWidth: 345,
+    margin: 10
+  },
+  media: {
+    height: 140,
+  },
+  actions: {
+    display: 'flex',
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+});
 
 class App extends Component {
+  constructor() {
+    super();
+    let now = new Date();
+    this.state = {
+      open: false,
+      daySchedule: {
+        'Wake Up': {1:'',2:'',3:''},
+        'Mid-Morning': {1:'',2:'',3:''},
+        'Afternoon': {1:'',2:'',3:''},
+        'Early Evening': {1:'',2:'',3:''},
+        'Night': {1:'',2:'',3:''},
+      },
+      endOfDay: '',
+      date1: now.getTime(),
+      date2: now.getTime(),
+      dialogBody: '',
+      popupTitle: '',
+    }
+  }
+
+  getDate = () => {
+    return (new Date()).toISOString().replace(/-|:|\.\d\d\d/g,"");
+  }
+
+  buildEventString = () => {
+    let title = 'Journaling+for+Emily';
+    let details = 'Remember+to+visit+linder.odinolav.com';
+    let startTime = this.state.date1;
+    let endTime = this.state.date2;
+    return `https://calendar.google.com/calendar/r/eventedit?dates=${startTime}/${endTime}&location&text=${title}&details=${details}`;
+  }
+
+  handleChange = name => event => {
+    console.log(event.target.value);
+    this.setState({[name]: event.target.value});
+  }
+
+  handleChangeActivity = (timeOfDay, colIndex) => event => {
+    let currentDaySchedule = this.state.daySchedule;
+    currentDaySchedule[timeOfDay][colIndex] = event.target.value;
+    this.setState(prevState => ({
+      daySchedule: currentDaySchedule
+    }));
+  }
+
+  showInputDescription = (title, desc) => event => {
+    this.setState({
+      dialogBody: desc,
+      popupTitle: title,
+      open: true
+    });
+  }
+
+  openInfoBox = () => {
+    this.setState({
+      dialogBody: <span><Typography component='span' variant='h6'>Background</Typography>
+          {STRINGS.background}
+          <Typography component='span' variant='h6'>Instructions</Typography>
+          {STRINGS.instructions}</span>,
+      popupTitle: 'Research Information',
+      open: true
+    });
+  }
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  LinderInput = (header, timeOfDay, i, classes, text) => {
+    return <TextField
+      key={`in-${timeOfDay}-${header}-${i}`}
+      id='outlined-controlled'
+      label={header}
+      multiline
+      rowsMax='20'
+      fullWidth
+      value={text}
+      onChange={this.handleChangeActivity(timeOfDay, i)}
+      className={classes}
+      margin='normal'
+      helperText=''
+      variant='outlined'
+      onDoubleClick={this.showInputDescription(header, STRINGS.headerDescriptions[i])}
+    />
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+    const { classes } = this.props;
+    let { date1, date2 } = this.state;
+    let inputFields = [];
+    for (let [timeOfDay, rowValue] of Object.entries(this.state.daySchedule)) {
+      inputFields.push(
+        <Card className={classes.card} key={`c-${timeOfDay}`}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image={STRINGS.timeImages[timeOfDay]}
+              title='Card Image'
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">{timeOfDay}</Typography>
+              {STRINGS.headers.map((header, i) => {
+                return this.LinderInput(header, timeOfDay, i, classes.textField, rowValue[i])
+              })}
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              <Edit />
+            </Button>
+            <IconButton
+            className={classnames(classes.expand, {
+              [classes.expandOpen]: this.state.expanded,
+            })}
+            onClick={this.handleExpandClick}
+            aria-expanded={this.state.expanded}
+            aria-label="Show more"
           >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+            <ExpandMore />
+          </IconButton>
+          </CardActions>
+        </Card>
+      );
+    };
+
+    return (<div className={classes.root}>
+      <Typography component='h1' variant='h4'>{STRINGS.title}</Typography>
+      <Button variant='outlined' color='primary' onClick={this.openInfoBox}>
+        Project Info
+      </Button>
+      <Dialog
+          open={this.state.open}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={this.handleClose}
+          aria-labelledby='alert-dialog-slide-title'
+          aria-describedby='alert-dialog-slide-description'
+          scroll='body'
+        >
+          <DialogTitle id='alert-dialog-slide-title'>
+            {this.state.popupTitle}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id='alert-dialog-slide-description'>
+              {this.state.dialogBody}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color='primary'>Okay</Button>
+          </DialogActions>
+        </Dialog>
+
+      <form className={classes.container} noValidate='noValidate'>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <Grid container={true} className={classes.grid} justify='space-around'>
+            <DatePicker margin='normal' label='Weekly day 1' value={date1} onChange={this.handleChange('date1')}/>
+            <TimePicker margin='normal' label='Time 1' value={date1} onChange={this.handleChange('date1')}/>
+            <DatePicker margin='normal' label='Weekly day 2' value={date2} onChange={this.handleChange('date2')}/>
+            <TimePicker margin='normal' label='Time 2' value={date2} onChange={this.handleChange('date2')}/>
+          </Grid>
+        </MuiPickersUtilsProvider>
+        <Button variant='contained' href={this.buildEventString()} target='_blank' className={classes.button}>
+          add to calendar
+        </Button>
+
+        <Grid container className={classes.root} spacing={16} justify='center'>
+          {inputFields}
+        </Grid>
+
+        <Fab variant='extended' aria-label='Add' className={classes.fab}>
+         <Navigation className={classes.extendedIcon} />
+         Extended
+        </Fab>
+      </form>
+      <footer>{this.disclaimer}</footer>
+    </div>);
   }
 }
 
-export default App;
+export default withRoot(withStyles(styles)(App));
---
+++ src/text/Strings.js
@@ -0,0 +1,39 @@
+export default {
+  title: 'Daily Journaling',
+  headers: [
+    'Significant events',
+    'Affective state',
+    'Comments'
+  ],
+  headerDescriptions: [
+    'Significant things that happened. Describe activities and locations you were engaged in.',
+    'Describe, in as much detail as you can, your affective state.',
+    'Other comments?'
+  ],
+  timeImages: {
+    'Wake Up': './img/morning.jpg',
+    'Mid-Morning': 'img/midmorning.jpg',
+    'Afternoon': 'img/afternoon.jpg',
+    'Early Evening': 'img/earlyevening.jpg',
+    'Night': 'img/night.jpg',
+  },
+  endOfDayPrompt: 'End of the day reflection / other comments',
+  background: `My senior project will examine student perceptions of stress. In this study, we would like to find out how the higher
+    education experience affects students’ perception of stress. We want to find out what counts as “stressful” for American college
+    students by considering their subjective experience. This activity is known as a Day Reconstruction Method (DRM) which provides
+    more continuous information and is more narrative and story like qualitative information than a plan survey. By doing this activity, I
+    am hoping to gain a better understanding of how sorts of activities are associated with stress.
+    Your privacy is important to me and your personal information will not be attached to your responses. By filling out this sheet and participating
+    in the activity, you are consenting to participate in the study and agree that the purpose of this research has been satisfactorily
+    explained to you.`,
+  instructions: `I am asking you to track your daily activities and associated stress level. This should be done twice a week for three
+    weeks, for a total of six different days (unless otherwise specified by me). This form will be your guide. Please fill out each box,
+    preferably adding to it as the day progresses to keep responses more authentic and accurate. For each part of the day, fill out the
+    three categories to the best of your ability, being as thorough as you are able. Describe anything significant that happened
+    in that time frame (classes, meetings, interactions, etc.) and how you felt about it. Did it make you happy, excited, tired, stressed,
+    overwhelmed, etc? Please feel free to add whatever comes to your mind. The more the merrier. I have also provided a section for a
+    reflection on the entire day. Think back on your day and whether it was good, bad, stressful, normal, exciting, etc.`,
+  disclaimer: `At no time are you required to write about or answer anything that you don’t feel comfortable with. Luther College has as standing
+    committee, the HSRB, to which complaints or problems concerning any research project may be reported to The Dean’s Office,
+    Luther College, 563-387-1005 if they arise.`,
+};
