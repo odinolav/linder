de1d5371179873e1c562e1eeff3c93b6d10852c6
odinolav <odinolav@gmail.com>
2019-02-21 03:43:59 -0600

    Add name profile card, begin email system change

--- src/App.js
+++ src/App.js
@@ -37,13 +37,15 @@ import AFTERNOON from './img/afternoon.jpg';
 import EARLYEVENING from './img/earlyevening.jpg';
 import NIGHT from './img/night.jpg';
 import EMILY from './img/emily.jpg';
+import PROFILE from './img/profile.jpg';
 
 const cardImgMap = {
   'Wake Up': WAKEUP,
   'Mid-Morning': MIDMORNING,
   'Afternoon': AFTERNOON,
   'Early Evening': EARLYEVENING,
-  'Night': NIGHT
+  'Night': NIGHT,
+  'Name': PROFILE
 }
 
 function Transition(props) {
@@ -61,7 +63,7 @@ const styles = theme => ({
   },
   expandedCard: {
     width: 350,
-    margin: 0
+    margin: 4
   },
   phoneCard: {
     width: 150,
@@ -123,6 +125,7 @@ class App extends Component {
         'Early Evening': {1:'',2:'',3:''},
         'Night': {1:'',2:'',3:''},
       },
+      'Name_Expanded': false,
       'Wake Up_Expanded': false,
       'Mid-Morning_Expanded': false,
       'Afternoon_Expanded': false,
@@ -131,6 +134,7 @@ class App extends Component {
       dialogBody: '',
       popupTitle: '',
       name: '',
+      nameComplete: false,
       dialogActions: this.OkayDialogButton(),
       eventString: `https://calendar.google.com/calendar/r/eventedit?text=Journaling+for+Emily+Linder&location=Decorah,+IA&details=Make+sure+to+visit+odinolav.com/linder`
     }
@@ -140,7 +144,9 @@ class App extends Component {
     let today = this.getReadableDate();
     if (localStorage['daySchedule_'+today]) {
       this.setState({
-        daySchedule: JSON.parse(localStorage['daySchedule_'+today])
+        daySchedule: JSON.parse(localStorage['daySchedule_'+today]),
+        name: localStorage.name ? localStorage.name : '',
+        nameComplete: localStorage.nameComplete
       });
     }
   }
@@ -154,6 +160,8 @@ class App extends Component {
     window.addEventListener("beforeunload", (ev) => {
        ev.preventDefault();
        localStorage['daySchedule_'+today] = JSON.stringify(this.state.daySchedule);
+       localStorage['name'] = this.state.name;
+       localStorage['nameComplete'] = this.state.nameComplete;
     });
    };
 
@@ -186,6 +194,29 @@ class App extends Component {
      return msg;
    }
 
+   makeEmailMessage = () => {
+     let text = '';
+     let html = [];
+     for (let [timeOfDay, responseObj] of Object.entries(this.state.daySchedule)) {
+       text += timeOfDay + '%0A';
+       html.push(<Typography key={timeOfDay} component='span' variant='h5'>{timeOfDay}</Typography>);
+       for (let [header, val] of Object.entries(responseObj)) {
+         if (val) {
+           text += `   ${STRINGS.headers[header]}%0A      ${val}%0A`;
+           html.push(
+            <span key={header+timeOfDay}>
+              <Typography component='span' variant='h6'>{STRINGS.headers[header]}</Typography>
+              <Typography component='span' variant='body1'>{val}</Typography>
+              <br />
+            </span>);
+         }
+       }
+       html.push(<br key={'br'+timeOfDay} />);
+       text += '%0A';
+     }
+     return {text, html};
+   }
+
   getDate = () => {
     return (new Date()).toISOString().replace(/-|:|\.\d\d\d/g,"");
   }
@@ -204,13 +235,19 @@ class App extends Component {
     });
   }
 
+  handleNameChange = event => {
+    let newVal = event.target.value;
+    this.setState({name: newVal, nameComplete: (newVal.length > 4 && newVal.trim().split(' ').length > 1)});
+  }
+
   handleChangeActivity = (timeOfDay, colIndex) => event => {
     let currentDaySchedule = this.state.daySchedule;
     currentDaySchedule[timeOfDay][colIndex] =
         event.target.value.replace(/--/, '—').replace(/\(\)/, '•').replace(/:\)/, '🙂').replace(/:D/, '😃').replace(/:\(/, '😔').replace(/:o/, '😮');
-    this.setState(prevState => ({
+
+    this.setState({
       daySchedule: currentDaySchedule
-    }));
+    });
   }
 
   handleExpandClick = (timeOfDay) => {
@@ -229,6 +266,22 @@ class App extends Component {
       open: true
     });
   }
+
+  showMailForm = () => {
+    let {text, html} = this.makeEmailMessage();
+    this.setState({
+      dialogBody:
+        <DialogContent>
+        <DialogContentText id='alert-dialog-slide-description'>
+          {html}
+        </DialogContentText>
+      </DialogContent>,
+      popupTitle: 'Submit to Emily',
+      dialogActions: this.OkayDialogButton(),
+      open: true
+    });
+  }
+
 
   openInfoBox = () => {
     this.setState({
@@ -254,18 +307,6 @@ class App extends Component {
         </DialogContentText></DialogContent>,
       popupTitle: 'Disclaimer',
       dialogActions: this.OkayDialogButton(),
-      open: true
-    });
-  }
-
-  openInputBox = (timeOfDay, rowValue, classes) => {
-    let body = STRINGS.headers.map((header, i) => {
-      return this.LinderInput(header, timeOfDay, i, classes, rowValue[i])
-    });
-
-    this.setState({
-      dialogBody: body,
-      popupTitle: timeOfDay,
       open: true
     });
   }
@@ -312,7 +353,7 @@ class App extends Component {
       }
       mainCards.push(
         <Card className={cardClass} key={`c-${timeOfDay}`}>
-          <CardActionArea>
+          <CardActionArea onClick={()=>this.handleExpandClick(timeOfDay)}>
             <CardMedia
               className={classes.media}
               image={cardImgMap[timeOfDay]}
@@ -345,7 +386,7 @@ class App extends Component {
         </Card>
       );
     };
-
+    // old fab href: href={`mailto:${SECRET_STRINGS.targetEmail}?subject=Research Journal for ${this.state.name} ${this.getHumanDate()}&body=${this.encodeEmail()}`}
     return (<div id='app'>
     <div id='mainarea'>
       <Dialog
@@ -366,6 +407,52 @@ class App extends Component {
 
       <form className={classes.container} noValidate='noValidate'>
         <Grid container className={classes.root} justify='center' alignItems='center'>
+
+          <Card className={isMobile ? classes.phoneCard : classes.card} key='k.1'>
+            <CardActionArea onClick={()=>this.handleExpandClick('Name')}>
+              <CardMedia
+                className={classes.media}
+                image={cardImgMap['Name']}
+                title='Your info'
+              />
+              <CardContent>
+                <Typography gutterBottom variant={isMobile ? "h6": "h5"} component="h2">Profile</Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Chip color={this.state.nameComplete ? 'primary' : 'secondary'} label={this.state.nameComplete ? 'Ready' : 'Incomplete'}/>
+              <IconButton
+                className={classnames(classes.expand, {
+                  [classes.expandOpen]: this.state['Name_Expanded'],
+                })}
+                onClick={()=>this.handleExpandClick('Name')}
+                aria-expanded={this.state['Name_Expanded']}
+                aria-label="Show more"
+              >
+                <Edit />
+              </IconButton>
+            </CardActions>
+            <Collapse in={this.state['Name_Expanded']} timeout="auto" unmountOnExit>
+            <CardContent>
+              <TextField
+                autoFocus
+                name='namefield'
+                id='outlined-controlled'
+                multiline
+                fullWidth
+                rowsMax='3'
+                value={this.state.name}
+                placeholder='your full name'
+                onChange={e => this.handleNameChange(e)}
+                margin='dense'
+                helperText=''
+                type='text'
+                variant='standard'
+              />
+            </CardContent>
+          </Collapse>
+          </Card>
+
           {mainCards}
         </Grid>
       </form>
@@ -384,8 +471,8 @@ class App extends Component {
           <Tooltip title="Send to Emily">
             <Fab
               className={classes.emilyButton}
-              href={`mailto:${SECRET_STRINGS.targetEmail}?subject=Research Journal for ${this.state.name} ${this.getHumanDate()}&body=${this.encodeEmail()}`}
               target='_blank'
+              onClick={this.showMailForm}
               >
               <Avatar alt="Emily Linder" src={EMILY} className={classes.avatar}/>
             </Fab>
